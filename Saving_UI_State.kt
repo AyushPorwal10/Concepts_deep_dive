9c96e306fe005e14026cf102e73d5d51819b3eab
Ayush Porwal <147789816+AyushPorwal10@users.noreply.github.com>
2025-09-15 20:05:37 +0530

    Create Saving_UI_State.kt

---
+++ Saving_UI_State.kt
@@ -0,0 +1,10 @@
+For jetpack compose we have remeberSaveable 
+Views: onSaveInstanceState() API.
+ViewModels: SavedStateHandle.
+
+Local storage to persist the UI state during app and activity transitions.
+
+
+
+Restrict activity recreation = https://developer.android.com/guide/topics/resources/runtime-changes#restrict-activity
+
