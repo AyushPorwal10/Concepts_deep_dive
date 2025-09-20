cef4b30e4708be02336896dc959cba955073d15a
Ayush Porwal <147789816+AyushPorwal10@users.noreply.github.com>
2025-09-21 01:00:17 +0530

    Update Saving_UI_State.kt

--- Saving_UI_State.kt
+++ Saving_UI_State.kt
@@ -8,3 +8,9 @@ Local storage to persist the UI state during app and activity transitions.
 
 Restrict activity recreation = https://developer.android.com/guide/topics/resources/runtime-changes#restrict-activity
 
+
+This post explain about onSaveInstanceState and onRestoreInstanceState
+https://www.linkedin.com/posts/outcomeschool_outcomeschool-softwareengineer-tech-activity-7301985158608392193-pA5M/
+
+Medium article for 
+https://medium.com/@shobhith/onsaveinstancestate-and-onrestoreinstancestate-in-android-c29f22b0ef4e
